49513aa163bf334993e61165cb3123e76ea71c92
superdxhua <superdxh@163.com>
2026-03-12 22:38:16 +0800

    Update ai-tcm.module.ts

--- server/src/ai-tcm/ai-tcm.module.ts
+++ server/src/ai-tcm/ai-tcm.module.ts
@@ -3,7 +3,6 @@ import { AiTcmController } from './ai-tcm.controller';
 import { AiTcmService } from './ai-tcm.service';
 
 @Module({
-  // 删除 imports: [LLMModule]
   controllers: [AiTcmController],
   providers: [AiTcmService],
 })
