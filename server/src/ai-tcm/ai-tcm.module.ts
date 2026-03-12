f617e31f3e850dbc1148376d5fee7f930b0ec429
superdxhua <superdxh@163.com>
2026-03-12 14:55:12 +0800

    Update ai-tcm.module.ts

--- server/src/ai-tcm/ai-tcm.module.ts
+++ server/src/ai-tcm/ai-tcm.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AiTcmController } from './ai-tcm.controller';
 import { AiTcmService } from './ai-tcm.service';
+import { LLMModule } from '../llm/llm.module'; // 引入 LLMModule
 
 @Module({
+  imports: [LLMModule], // 导入 LLMModule
   controllers: [AiTcmController],
   providers: [AiTcmService],
 })
