05533e76df7e88d80771f8ed6d0d5e139d043dd3
superdxhua <superdxh@163.com>
2026-03-13 14:28:51 +0800

    Update ai-tcm.module.ts

--- server/src/ai-tcm/ai-tcm.module.ts
+++ server/src/ai-tcm/ai-tcm.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AiTcmController } from './ai-tcm.controller';
 import { AiTcmService } from './ai-tcm.service';
+import { MedicalAiModule } from '../medical-ai/medical-ai.module'; // 引入 MedicalAiModule
 
 @Module({
+  imports: [MedicalAiModule], // 导入 MedicalAiModule
   controllers: [AiTcmController],
   providers: [AiTcmService],
 })
